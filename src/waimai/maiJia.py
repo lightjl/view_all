fa2ad90af875af1c5f8b2da4c1fa21f9004e5587
jlgs-jz <jlgs-jz@192.168.16.32>
2017-10-12 22:42:34 +0800

    add 外卖at li

---
+++ src/waimai/maiJia.py
@@ -0,0 +1,65 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import sendMail
+
+class maiJia:
+    def __init__(self, name, link):
+        self.name = name
+        self.link = link
+        self.id = link.split('/')[-1]
+        # print(link)
+        # print(name + ' ' + self.id)
+        
+    def findFood(self, priceTotal = 50):
+        browser = webdriver.Firefox()
+        baseUrl = 'https://h5.ele.me/shop/#geohash=ws0ed952uqk9&id='
+        linkM = baseUrl + str(self.id)
+        browser.get(linkM)
+        # /html/body/div[1]/div/div/div[3]/div[2]/div/footer/div[3]/a/span
+        time.sleep(2)
+        priceStr = browser.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div[2]/div/footer/div[3]/a/span').text
+        priceMin = int(priceStr.split('¥')[1].split('起')[0])
+        sendPriceStr = browser.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div[2]/div/footer/div[3]/div/p[2]').text
+        if (len(sendPriceStr) == 0):
+            sendPrice = -1
+        else:
+            sendPrice = float(sendPriceStr.split('¥')[1])
+        # print(priceMin)
+        time.sleep(1)
+        browser.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div[2]/main/ul/li[2]/span').click()
+        time.sleep(3)
+        youhuiItems = browser.find_elements_by_xpath('/html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[2]/dd')
+        text = ''
+        textmin = ''
+        for youhuiItem in youhuiItems:
+            youhuiItem.send_keys(Keys.DOWN)
+            nameItem = youhuiItem.find_element_by_xpath('./div/div/section/header/span').text
+            priceItem = youhuiItem.find_elements_by_xpath('./div/div/section/strong/span')
+            price = float(priceItem[0].text)
+            if (price  >= priceMin and price + sendPrice <= priceTotal and price >= 2):
+                # print(nameItem + str(price))
+                text += (nameItem + ' ' + str(price)) + '\n'
+            elif (price < 9 and price < priceMin):
+                textmin += (nameItem + ' ' + str(price)) + '\n'
+                
+        if len(text) > 0:
+            print(self.name + ' 配送费 ' + str(sendPrice))
+            print(linkM + '\n' + text + textmin)
+            sendMail.sendMail('wm: ' + self.name + ' 配送费 ' + str(sendPrice), linkM + '\n' + text)
+        browser.quit()
+            # /div/div/section/header/span
+            # ./div/div/section/header/span
+            # ./div/div/section/p[1]
+        # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[1]/dd[7]/div/div/section/strong/span[1]
+        # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[4]/dd[2]/div/div/section/strong/span[1]
+        # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[2] /dd[5]/div/div/section/strong/span[1]
+        # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[2]/dd[1]/div/div/section/strong/span
+        # 起
+        # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[1]/dd[10]/div/div/section/strong/span
+        # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dll[11]/dd[1]/div/div/section/strong/span
+        
+        
+        # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[2]/dd[1]/div/div/section/strong/span
+        # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[2]/dd[1]/div/div/section/strong/del
+        
---
+++ src/waimai/waimai.py
@@ -0,0 +1,120 @@
+from lxml import etree
+import requests
+import sendMail
+import recMail
+import time
+import WorkInTime
+import logging
+from selenium import webdriver
+import maiJia
+from pydoc import browse
+from multiprocessing import Process, Value
+import threading
+import os
+
+#timeB = [['19:46', '23:00']]
+
+def findFood(maiJiaList):
+    for maiJia in maiJiaList:
+        maiJia.findFood(46)
+
+def gundong(browser):
+    for i in range(10):
+        js="document.documentElement.scrollTop=1000000"
+        browser.execute_script(js)
+        time.sleep(1)
+                    
+def pinpai(browser):
+    # '/html/body/div/ul/section[1]/div[2]/section[1]/h3/span'
+    # '/html/body/div/ul/section[2]/div[2]/section[1]/h3/span'
+    dians = browser.find_elements_by_xpath('/html/body/div/ul/section')
+    diansList = []
+    for dian in dians:
+        name = dian.find_element_by_xpath('./div[2]/section[1]/h3/span').text
+        diansList.append(name)
+    return diansList
+    
+def findMaijia():
+    browser = webdriver.Firefox()
+    url = 'https://h5.ele.me/msite/food/#geohash=ws0ed952uqk9&#target={"category_schema":%20{"category_name":%20"\u7f8e\u98df",%20"complex_category_ids":%20[207,%20220,%20233,%20260],%20"is_show_all_category":%20false},%20"restaurant_category_id":%20[209,%20211,%20212,%20213,%20214,%20215,%20216,%20217,%20218,%20219,%20221,%20222,%20223,%20224,%20225,%20226,%20227,%20228,%20229,%20230,%20231,%20232,%20234,%20235,%20236,%20237,%20238,%20263,%20264,%20265,%20266,%20267,%20268,%20269]}&target_name=美食&animation_type=1&banner_type=0&business_flag=1&color_type=1&entry_id=15&search_source=1'
+    browser.get(url)
+    # 品牌
+    time.sleep(2)
+    browser.find_element_by_xpath('/html/body/div/div[1]/div/aside/div/a[3]').click()
+    time.sleep(2)
+    browser.find_element_by_xpath('/html/body/div/div[1]/div/aside/section[3]/div[1]/dl[4]/dd[1]').click()
+    browser.maximize_window()
+    time.sleep(2)
+    browser.find_element_by_xpath('/html/body/div/div[1]/div/aside/section[3]/div[2]/a[2]').click()
+    time.sleep(6)
+    # 晚餐
+    '''
+    browser.find_element_by_xpath('/html/body/div/div[1]/div/aside/div/a[1]').click()
+    time.sleep(6)
+    browser.find_element_by_xpath('/html/body/div/div[1]/div/aside/section[1]/div/ul[1]/li[15]').click()
+    time.sleep(2)
+    browser.find_element_by_xpath('/html/body/div/div[1]/div/aside/section[1]/div/ul[2]/li[1]').click()
+    time.sleep(4)
+    '''
+    
+    gundong(browser)
+    diansList = pinpai(browser)
+    url = 'https://www.ele.me/place/ws0ed952uqk?latitude=23.12089&longitude=113.31764'
+    browser.get(url)
+    time.sleep(4)
+    browser.find_element_by_xpath('/html/body/div[4]/div[3]/div[1]/div/a[15]').click()
+
+    
+    #print(html.text)
+    # /html/body/div[4]/div[3]/div[2]/div[1]/a[1]/div[2]/div[4]/i
+    # /html/body/div[4]/div[3]/div[2]/div[1]/a[3]/div[2]/div[4]/i[2]
+    
+    # /html/body/div[4]/div[3]/div[2]/div[1]/a[1] /div[2]/div[1] 店名
+    
+    # /html/body/div[4]/div[3]/div[2]/div[1]/a[6]
+    gundong(browser)
+    time.sleep(1)
+    # print(diansList)
+    
+    maijias = browser.find_elements_by_xpath('/html/body/div[4]/div[3]/div[2]/div[1]/a')
+    maijiaList = []
+    for maijia in maijias:
+        link = (maijia.get_attribute("href"))
+        name = (maijia.find_element_by_xpath('./div[2]/div[1]').text)
+        if name not in diansList:
+            continue
+        tzs = maijia.find_elements_by_xpath('./div[2]/div[4]/i')
+        mj = maiJia.maiJia(name, link)
+        maijiaList.append(mj)
+    browser.quit()
+    findFood(maijiaList)
+    
+runFlag = Value('b', True)
+
+def checkRunFlag():
+    module_path = os.path.dirname(__file__)
+    filename = module_path + '/waimai.pyrunable.txt'
+    file_object = open(filename, 'r')
+    try:
+        flag_run = file_object.read()
+    finally:
+        file_object.close()
+    while flag_run == 'True':
+        time.sleep(5)
+        file_object = open(filename, 'r')
+        try:
+            flag_run = file_object.read()
+        finally:
+            file_object.close()
+    runFlag.value = False
+    
+if __name__ == '__main__':
+    timeTrade = [['16:00', '16:10']]
+    workTime = WorkInTime.WorkInTime(timeTrade, weekday='0,1,2,3,4')
+    checkRun = threading.Thread(target=checkRunFlag, args=())
+    checkRun.start()
+    while runFlag.value:
+        relaxNow = threading.Thread(target=workTime.relax, args=(runFlag,'Mtime'))
+        relaxNow.start()
+        relaxNow.join()
+        findMaijia()
