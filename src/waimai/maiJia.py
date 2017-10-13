2b93a083ea00ec19ca8406e6294bc1460de15b1e
jlgs-jz <jlgs-jz@192.168.16.32>
2017-10-13 19:15:13 +0800

    扩大搜索范围，修正起购价计算，去掉‘已售完’外卖

--- src/waimai/maiJia.py
+++ src/waimai/maiJia.py
@@ -17,7 +17,7 @@ class maiJia:
         linkM = baseUrl + str(self.id)
         browser.get(linkM)
         # /html/body/div[1]/div/div/div[3]/div[2]/div/footer/div[3]/a/span
-        time.sleep(2)
+        time.sleep(4)
         priceStr = browser.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div[2]/div/footer/div[3]/a/span').text
         priceMin = int(priceStr.split('¥')[1].split('起')[0])
         sendPriceStr = browser.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div[2]/div/footer/div[3]/div/p[2]').text
@@ -27,21 +27,35 @@ class maiJia:
             sendPrice = float(sendPriceStr.split('¥')[1])
         # print(priceMin)
         time.sleep(1)
-        browser.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div[2]/main/ul/li[2]/span').click()
-        time.sleep(3)
-        youhuiItems = browser.find_elements_by_xpath('/html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[2]/dd')
         text = ''
         textmin = ''
-        for youhuiItem in youhuiItems:
-            youhuiItem.send_keys(Keys.DOWN)
-            nameItem = youhuiItem.find_element_by_xpath('./div/div/section/header/span').text
-            priceItem = youhuiItem.find_elements_by_xpath('./div/div/section/strong/span')
-            price = float(priceItem[0].text)
-            if (price  >= priceMin and price + sendPrice <= priceTotal and price >= 2):
-                # print(nameItem + str(price))
-                text += (nameItem + ' ' + str(price)) + '\n'
-            elif (price < 9 and price < priceMin):
-                textmin += (nameItem + ' ' + str(price)) + '\n'
+        items = []
+        for ith in range(1,4):
+            browser.find_element_by_xpath('/html/body/div[1]/div/div/div[3]/div[2]/main/ul/li[%i]/span' % ith).click()
+            time.sleep(3)
+            youhuiItems = browser.find_elements_by_xpath('/html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[%i]/dd' % ith)
+            for youhuiItem in youhuiItems:
+                youhuiItem.send_keys(Keys.DOWN)
+                if (youhuiItem.find_element_by_xpath('./div/div/section/div/span/span').text == '已售完'):
+                    continue
+                nameItem = youhuiItem.find_element_by_xpath('./div/div/section/header/span').text
+                priceItem = youhuiItem.find_element_by_xpath('./div/div/section/strong')
+                price = float(priceItem.find_element_by_xpath('./span').text)
+                priceDel = price
+                try:
+                    priceDel = float(priceItem.find_element_by_xpath('./del').text.replace('¥',''))
+                except:
+                    priceDel = price
+                    pass
+                if nameItem in items:
+                    continue
+                if (priceDel  >= priceMin and price + sendPrice <= priceTotal and price >= 2):
+                    # print(nameItem + str(price))
+                    text += (nameItem + ' ' + str(price)) + '\n'
+                    items.append(nameItem)
+                elif (price < 9 and price < priceMin):
+                    textmin += (nameItem + ' ' + str(price)) + '\n'
+                    items.append(nameItem)
                 
         if len(text) > 0:
             print(self.name + ' 配送费 ' + str(sendPrice))
@@ -51,10 +65,10 @@ class maiJia:
             # /div/div/section/header/span
             # ./div/div/section/header/span
             # ./div/div/section/p[1]
-        # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[1]/dd[7]/div/div/section/strong/span[1]
-        # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[4]/dd[2]/div/div/section/strong/span[1]
-        # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[2] /dd[5]/div/div/section/strong/span[1]
+        # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[2]/dd[3]/div/div/section/div/span/span (已售完)
         # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[2]/dd[1]/div/div/section/strong/span
+        # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[2]/dd[18]/div/div/section/strong/span
+        # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[2]/dd[18]/div/div/section/strong/del
         # 起
         # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dl[1]/dd[10]/div/div/section/strong/span
         # /html/body/div[1]/div/div/div[3]/div[2]/main/section/div[1]/dll[11]/dd[1]/div/div/section/strong/span
