53f2120c361fb55a6686f07c0bd33542a4d4f447
jlgs-jz <jlgs-jz@192.168.16.32>
2017-10-14 00:20:56 +0800

    增加btbtdy追电影，ini指名和邮箱

---
+++ src/dygxtx/moiveE.py
@@ -0,0 +1,104 @@
+import imMail
+import logging
+import sendMail
+import requests
+from lxml import etree
+from selenium import webdriver
+import pandas as pd
+import os
+
+# 判断一个unicode是否是英文字母
+def is_alphabet(uchar):         
+    if ('\u0041' <= uchar<='\u005a') or ('\u0061' <= uchar<='\u007a'):
+        return True
+    else:
+        return False
+    
+class MoiveWait():
+    def __init__(self, dyName, receiver, foundFlag):
+        self.dyName = dyName
+        self.receiver = receiver
+        self.foundFlag = foundFlag
+        
+    def DownloadLink(self, link):
+        session = requests.Session()
+        headers = {
+            'User-Agent':'Mozilla/5.0 (Windows; U; Windows NT 6.1; en-US; rv:1.9.1.6) Gecko/20091201 Firefox/3.5.6'
+        }
+        f = session.get(link, headers = headers)
+        selector = etree.HTML(f.text)
+        # /html/body/div[4]/div/div/ul[1]/li/div[2]/a[1]
+        # /html/body/div[4]/div/div/ul[1]/li/div[2]/a[1]
+        # self.ed2kLink = selector.xpath('/html/body/div[4]/div/div/ul[1]/li/div[2]/a[1]/@href')
+        driver = webdriver.PhantomJS()
+        driver.get(link)
+        plists = driver.find_elements_by_xpath('//div[@id="nucms_downlist"]/div')
+        downloadLink = ''
+        minSize = 999
+        # //*[@id="nucms_downlist"]/div[1]
+        for plist in plists:
+            lis = plist.find_elements_by_xpath('./ul/li')
+            
+            for li in lis:
+                # //*[@id="nucms_downlist"]/div/ul/li[1] /a
+                sizeStr = li.find_element_by_xpath('./a').get_attribute('title').split('[')[1].split(']')[0]
+                if ('G' not in sizeStr):    # 大小不是GB 级别
+                    continue
+                size = float(sizeStr.split('G')[0])
+                if (size < minSize):
+                    minSize = size
+                    logging.debug(size)
+                    # //*[@id="nucms_downlist"]/div/ul/li[1] /span/a
+                    # //*[@id="nucms_downlist"]/div[1]/ul/li/span/a
+                    downloadLink = li.find_element_by_xpath('./span/a').get_attribute('href')
+        driver.quit()
+        return downloadLink
+        
+class Moives:
+    def __init__(self, dyPd):
+        self.moiveList = []
+        self.nameList = []
+        self.dyPd = dyPd
+        for ith in dyPd.values:
+            if ith[2] == 'F':
+                self.addWaitMoive(ith[0], ith[1], ith[2])
+        logging.debug(self.dyPd)
+                
+    def addWaitMoive(self, dyName, receiver, foundFlag):
+        moiveWait = MoiveWait(dyName, receiver, foundFlag)
+        self.moiveList.append(moiveWait)
+        self.nameList.append(dyName)
+        
+    def sendMail(self, moiveWait, link):
+        logging.info('mv: ' + moiveWait.dyName)
+        logging.info(moiveWait.DownloadLink(link))
+        sendMail.sendMail('mv: ' + moiveWait.dyName, moiveWait.DownloadLink(link))#, receiver='presouce@163.com', sendFrom='163')
+        if (moiveWait.receiver == moiveWait.receiver):
+            sendMail.sendMail('mv: ' + moiveWait.dyName, moiveWait.DownloadLink(link), receiver=moiveWait.receiver, sendFrom='163')
+        else:
+            logging.debug('only send to myself')
+    
+    def reWriteIni(self):
+        module_path = os.path.dirname(__file__)
+        filename = module_path + '/ini.csv'
+        self.dyPd.to_csv(filename, encoding='utf-8', index=False)
+    
+    def check(self, dyName, link):
+        if(dyName in self.nameList):
+            for moive in self.moiveList:
+                if(moive.dyName == dyName):
+                    logging.debug(self.dyPd.loc[self.dyPd['name'] == dyName , 'foundFlag'])
+                    self.dyPd.loc[self.dyPd['name'] == dyName , 'foundFlag'] = 'T'
+                    self.sendMail(moive, link)
+                    self.reWriteIni()
+                    return
+    
+
+           
+''' 
+driver = webdriver.PhantomJS()
+driver.get('http://www.btbtdy.com/btdy/dy11658.html#download')
+link = driver.find_element_by_xpath('//*[@id="nucms_downlist"]/div[1]/ul/li/span/a')
+print(link.get_attribute('href'))
+driver.quit()
+'''
---
+++ src/dygxtx/zdy.py
@@ -0,0 +1,96 @@
+import requests
+from bs4 import BeautifulSoup
+import pandas as pd
+import re
+import WorkInTime
+from lxml import etree
+import moiveE
+import logging
+import time
+from multiprocessing import Process, Value
+import threading
+import os
+
+logging.basicConfig(level=logging.CRITICAL, format='%(asctime)s - %(levelname)s -%(message)s')
+
+def checkBtDy(dyWait):  # 
+    session = requests.Session()
+    #print(login_session.status_code)
+    #print(_cookies.get_dict())
+    site = 'http://www.btbtdy.com'
+    url = 'http://www.btbtdy.com/btfl/dy1.html'
+    time.sleep(5)
+    f = session.get(url)
+    selector = etree.HTML(f.text)
+    # print(selector)
+    ## /html/body/div[4]/div/div/div[2]/div/ul/li[1] /div[2]/ul/li[1] /a[1]/span
+    # /html/body/div[4]/div/div/div[2]/div/ul/li[1] /div[2]/ul/li[1] /div/div/div/a[2]
+    ## /html/body/div[4]/div/div/div[2]/div/ul/li[1]/div[2]/ul/li[2]/a[1]/span
+    # /html/body/div[4]/div/div/div[2]/div/ul/li[1]/div[2]/ul/li[2]/div/div/div/a[2]
+    
+    # /html/body/div[4]/div/div/div[2]/div/ul/li[2]/div[2]/ul/li[1]/a[1]/span
+    content_field = selector.xpath('//div[@class="liimg"]')
+    for each in content_field:
+        # /html/body/div[4]/ul/li[2]/div[1]  /a
+        moiveName = each.xpath('./a/@title')[0]
+        link = site + each.xpath('./a/@href')[0]
+        banben = each.xpath('./a/span/text()')[0]
+        if (banben == '预告片' or banben == '抢先版'):
+            continue
+        # /html/body/div[4]/ul/li[2]/div[1]  /a/span
+        souce = each.xpath('../div[2]/p[1]/span/text()')[0]
+        # /html/body/div[4]/ul/li[2]/div[2]/p[1]/span
+        '''
+        logging.debug(moiveName)
+        logging.debug(link)
+        logging.debug(banben + ' ' + souce)
+        '''
+        dyWait.check(moiveName, link)
+    # print(f.content.decode())
+
+    logging.critical('wait moive(s)')
+
+runFlag = Value('b', True)
+
+def checkRunFlag():
+    module_path = os.path.dirname(__file__)
+    filename = module_path + '/zdy.pyrunable.txt'
+    file_object = open(filename, 'r')
+    try:
+        flag_run = file_object.read()
+    finally:
+        file_object.close()
+    while flag_run == 'True':
+        time.sleep(5)
+        file_object = open(filename, 'r')
+        try:
+            flag_run = file_object.read()
+        finally:
+            file_object.close()
+    runFlag.value = False
+    
+
+
+if __name__ == '__main__':
+    # 检查是否要运行
+    checkRun = threading.Thread(target=checkRunFlag, args=())
+    checkRun.start()
+    
+    
+    timeBucket =[['17:29', '17:32']]
+    
+    workTime = WorkInTime.WorkInTime(timeBucket, relaxTime=60*10)
+    
+    while runFlag.value:
+        relaxNow = threading.Thread(target=workTime.relax, args=(runFlag,))
+        relaxNow.start()
+        relaxNow.join()
+        
+        module_path = os.path.dirname(__file__)
+        filename = module_path + '/ini.csv'
+        dyPd = pd.read_csv(filename)
+        dyWait = moiveE.Moives(dyPd)
+        if (len(dyWait.nameList) == 0):
+            logging.critical('no moives wait!')
+        else:
+            checkBtDy(dyWait)
